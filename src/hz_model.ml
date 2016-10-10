e891a246e7efa33ba98f8b49fb44a34d402bff09
Cyrus Omar <cyology@gmail.com>
2016-10-10 13:53:19 -0400

    removed cruft from hz_model.ml

--- src/hz_model.ml
+++ src/hz_model.ml
@@ -4,8 +4,6 @@ module Model = struct
   type t = ZExp.t * HTyp.t
 
   let empty = ((ZExp.CursorE HExp.EmptyHole), HTyp.Hole)
-  (* let empty = (ZExp.FocusedE (HExp.Plus (HExp.EmptyHole,HExp.EmptyHole)),  *)
-  (* HType.Num) *)
 
   (* react *)
   type rs = t React.signal (* reactive signal *)
