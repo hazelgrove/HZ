b971d8ffd320fc9c1b32dd90907058f4eb347dfa
Cyrus Omar <cyology@gmail.com>
2016-10-10 15:49:16 -0400

    found what was slowing ocp-indent down, added it back

--- src/build.sh
+++ src/build.sh
@@ -1,8 +1,8 @@
-# for f in `find . -name '*.ml*'` ; \
-# do ( \
-#	ocp-indent -i $f; \
-# ); \
-# done
+for f in `find . -name '*.ml'` ; \
+do ( \
+ocp-indent -i $f; \
+); \
+done
 
 ocamlbuild -use-ocamlfind \
   -pkgs js_of_ocaml,js_of_ocaml.ppx,js_of_ocaml.tyxml,tyxml,react,reactiveData \
--- src/fix-indent.sh
+++
@@ -1,6 +0,0 @@
-for f in `find . -name '*.ml*'` ; \
-do ( \
-ocp-indent -i $f; \
-); \
-done
-
