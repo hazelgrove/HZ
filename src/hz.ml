a92b28e065ff774e4bec9760dd7beaa09ca5f7b9
Michael Hilton <michael.hilton@gmail.com>
2016-10-10 09:38:18 -0700

    moved input boxes and add placeholder text

--- src/hz.ml
+++ src/hz.ml
@@ -101,45 +101,45 @@ module Util = struct
 end
 
 module JSUtil = struct
-  let listen_to ev elem f = 
+  let listen_to ev elem f =
     Dom_html.addEventListener
       elem
       ev
       (Dom_html.handler f)
       Js._false
 
-  let listen_to_t ev elem f = 
-    listen_to ev elem (fun evt -> 
+  let listen_to_t ev elem f =
+    listen_to ev elem (fun evt ->
         f evt; Js._true)
 
   (* create an input and a reactive signal tracking its
    * string value *)
   let r_input id =
     let rs, rf = S.create "" in
-    let i_elt = Html5.(input ~a:[a_id id; a_class ["form-control"]] ();  ) in
+    let i_elt = Html5.(input ~a:[a_id id; a_class ["form-control"]; a_placeholder "Enter var + press Enter"]  ();  ) in
     let i_dom = To_dom.of_input i_elt in
-    let _ = listen_to_t Ev.input i_dom (fun _ -> 
-        rf (Js.to_string (i_dom##.value))) in 
+    let _ = listen_to_t Ev.input i_dom (fun _ ->
+        rf (Js.to_string (i_dom##.value))) in
     ((rs, rf), i_elt, i_dom)
 end
 
 (* generates the action palette *)
 module ActionPalette = struct
-  let make_palette ((rs, rf) : Model.rp) = 
-    let doAction action = 
-      rf (Action.performSyn Ctx.empty action (React.S.value rs)) in 
+  let make_palette ((rs, rf) : Model.rp) =
+    let doAction action =
+      rf (Action.performSyn Ctx.empty action (React.S.value rs)) in
 
     (* helper function for constructing simple action buttons *)
     let action_button action btn_label key_code =
-      let _ = JSUtil.listen_to_t Ev.keypress Dom_html.document (fun evt -> 
-          if evt##.keyCode = key_code then 
-            try 
-              doAction action 
-            with Action.InvalidAction -> () 
-          else ()) in 
+      let _ = JSUtil.listen_to_t Ev.keypress Dom_html.document (fun evt ->
+          if evt##.keyCode = key_code then
+            try
+              doAction action
+            with Action.InvalidAction -> ()
+          else ()) in
       Html5.(button ~a:[a_class ["btn";"btn-outline-primary"];
                         a_onclick (fun _ ->
-                            doAction action; 
+                            doAction action;
                             true);
                         R.filter_attrib
                           (a_disabled ())
@@ -148,7 +148,7 @@ module ActionPalette = struct
                                  let _ = Action.performSyn Ctx.empty action m in false
                                with Action.InvalidAction -> true
                              ) rs)
-                       ] [pcdata btn_label]) in 
+                       ] [pcdata btn_label]) in
 
     (* actions that take an input. the conversion function
      * goes from a string to an arg option where arg is
@@ -156,16 +156,16 @@ module ActionPalette = struct
     let action_input_button action conv btn_label input_id key_code =
       (* create reactive input box *)
       let (i_rs, i_rf), i_elt, i_dom = JSUtil.r_input input_id in
-      let clear_input () = 
+      let clear_input () =
         i_dom##.value := (Js.string "");
-        i_rf "" in 
+        i_rf "" in
       let button_elt = Html5.(button ~a:[
-          a_class ["btn"; "btn-default"]; 
+          a_class ["btn"; "btn-default"];
           a_id (input_id ^ "_button");
           a_onclick (fun _ ->
               let arg = Util.opt_get (conv (React.S.value i_rs)) in
               doAction (action arg);
-              clear_input (); 
+              clear_input ();
               true
             );
           R.filter_attrib
@@ -178,40 +178,39 @@ module ActionPalette = struct
                      with Action.InvalidAction -> true
                    end
                  | _ -> true) i_rs rs)
-        ] [pcdata btn_label]) in 
-      let button_dom = To_dom.of_button button_elt in 
-      let _ = JSUtil.listen_to Ev.keypress Dom_html.document (fun evt -> 
-          let evt_key = evt##.keyCode in 
+        ] [pcdata btn_label]) in
+      let button_dom = To_dom.of_button button_elt in
+      let _ = JSUtil.listen_to Ev.keypress Dom_html.document (fun evt ->
+          let evt_key = evt##.keyCode in
           (* let _ = Firebug.console##log evt_key in *)
-          (* let _ = Firebug.console##log key_code in *) 
-          if evt_key = key_code then 
-            begin 
-              i_dom##focus; 
+          (* let _ = Firebug.console##log key_code in *)
+          if evt_key = key_code then
+            begin
+              i_dom##focus;
               Dom_html.stopPropagation evt;
               Js._false
             end
-          else 
+          else
             Js._true
-        ) in 
+        ) in
       let _ = JSUtil.listen_to Ev.keypress i_dom (fun evt ->
-          let evt_key = evt##.keyCode in 
-          match evt_key with 
+          let evt_key = evt##.keyCode in
+          match evt_key with
           | 13 -> begin
-              button_dom##click; 
-              i_dom##blur; 
+              button_dom##click;
+              i_dom##blur;
               Js._false
-            end 
-          | 27 -> begin 
-              i_dom##blur; 
+            end
+          | 27 -> begin
+              i_dom##blur;
               Js._false
             end
           | _ -> Dom_html.stopPropagation evt; Js._true
-        ) in 
+        ) in
       Html5.(div ~a:[a_class ["input-group"]] [
-          i_elt;
           span ~a:[a_class ["input-group-btn"]] [
-            button_elt]
-        ]) in 
+            button_elt];i_elt;
+        ]) in
 
     (* actions that takes two inputs. the conversion function
      * goes from a pair of strings to an arg option where arg is
@@ -221,18 +220,18 @@ module ActionPalette = struct
       let input_id_2 = (input_id ^ "_2") in
       let (i_rs_1, i_rf_1), i_elt_1, i_dom_1 = JSUtil.r_input input_id_1 in
       let (i_rs_2, i_rf_2), i_elt_2, i_dom_2 = JSUtil.r_input input_id_2 in
-      let clear_input () = 
-        i_dom_1##.value := (Js.string ""); i_rf_1 "";  
-        i_dom_2##.value := (Js.string ""); i_rf_2 "" in 
+      let clear_input () =
+        i_dom_1##.value := (Js.string ""); i_rf_1 "";
+        i_dom_2##.value := (Js.string ""); i_rf_2 "" in
       let button_elt = Html5.(button ~a:[
-          a_class ["btn"; "btn-default"]; 
+          a_class ["btn"; "btn-default"];
           a_id (input_id ^ "_button");
           a_onclick (fun _ ->
-              let i1 = React.S.value i_rs_1 in 
-              let i2 = React.S.value i_rs_2 in 
+              let i1 = React.S.value i_rs_1 in
+              let i2 = React.S.value i_rs_2 in
               let arg = Util.opt_get (conv (i1, i2)) in
               doAction (action arg);
-              clear_input (); 
+              clear_input ();
               true
             );
           R.filter_attrib
@@ -245,48 +244,48 @@ module ActionPalette = struct
                      with Action.InvalidAction -> true
                    end
                  | _ -> true) i_rs_1 i_rs_2 rs)
-        ] [pcdata btn_label]) in 
-      let button_dom = To_dom.of_button button_elt in 
-      let _ = JSUtil.listen_to Ev.keypress Dom_html.document (fun evt -> 
-          let evt_key = evt##.keyCode in 
-          if evt_key = key_code then 
-            begin 
-              i_dom_1##focus; 
+        ] [pcdata btn_label]) in
+      let button_dom = To_dom.of_button button_elt in
+      let _ = JSUtil.listen_to Ev.keypress Dom_html.document (fun evt ->
+          let evt_key = evt##.keyCode in
+          if evt_key = key_code then
+            begin
+              i_dom_1##focus;
               Dom_html.stopPropagation evt;
               Js._false
             end
-          else 
+          else
             Js._true
-        ) in 
+        ) in
       let i_keypress_listener i_dom = JSUtil.listen_to Ev.keypress i_dom (fun evt ->
-          let evt_key = evt##.keyCode in 
-          match evt_key with 
+          let evt_key = evt##.keyCode in
+          match evt_key with
           | 13 -> begin
-              button_dom##click; 
-              i_dom##blur; 
+              button_dom##click;
+              i_dom##blur;
               Js._false
-            end 
-          | 27 -> begin 
-              i_dom##blur; 
+            end
+          | 27 -> begin
+              i_dom##blur;
               Js._false
             end
           | _ -> Dom_html.stopPropagation evt; Js._true
-        ) in 
-      let _ = i_keypress_listener i_dom_1 in 
-      let _ = i_keypress_listener i_dom_2 in 
+        ) in
+      let _ = i_keypress_listener i_dom_1 in
+      let _ = i_keypress_listener i_dom_2 in
       Html5.(div ~a:[a_class ["input-group"]] [
-          i_elt_1;
-          i_elt_2; 
           span ~a:[a_class ["input-group-btn"]] [
-            button_elt]
-        ]) in 
+            button_elt];
+          i_elt_1;
+          i_elt_2;
+        ]) in
 
     Html5.(div ~a:[a_class ["row";"marketing"]] [
         div ~a:[a_class ["col-lg-3"; "col-md-3"; "col-sm-3"]] [
           div ~a:[a_class ["panel";"panel-default"]] [
             div ~a:[a_class ["panel-title"]] [pcdata "Movement"];
             div ~a:[a_class ["panel-body"]] [
-              (action_button (Action.Move (Action.Child 1)) "move child 1 (1)" 49);
+              (action_button (Action.Move (Action.Child 1)) "<b>move child 1 </b> (1)" 49);
               br ();
               (action_button (Action.Move (Action.Child 2)) "move child 2 (2)" 50);
               br ();
@@ -328,17 +327,17 @@ module ActionPalette = struct
             div ~a:[a_class ["panel-body"]] [
               (action_button (Action.Construct Action.SAsc) "construct asc (:)" 58);
               br ();
-              (action_input_button 
+              (action_input_button
                  (fun v -> Action.Construct (Action.SVar v))
                  (fun s -> match String.compare s "" with 0 -> None | _ -> Some s)
                  "construct var (v)" "var_input" 118);
-              (action_input_button 
+              (action_input_button
                  (fun v -> Action.Construct (Action.SLam v))
                  (fun s -> match String.compare s "" with 0 -> None | _ -> Some s)
                  "construct lam (\\)" "lam_input" 92);
               (action_button (Action.Construct Action.SAp) "construct ap ( ( )" 40);
               br ();
-              (action_input_button  
+              (action_input_button
                  (fun n -> Action.Construct (Action.SLit n))
                  (fun s -> try Some (int_of_string s) with Failure _ -> None)
                  "construct lit (#)" "lit_input" 35);
@@ -348,14 +347,14 @@ module ActionPalette = struct
               br ();
               (action_button (Action.Construct (Action.SInj HExp.R)) "construct inj R (r)" 114);
               br ();
-              (action_input_input_button  
+              (action_input_input_button
                  (fun (v1,v2) -> Action.Construct (Action.SCase (v1,v2)))
-                 (fun (s1, s2) -> 
-                    let s1_empty = String.compare s1 "" in 
-                    let s2_empty = String.compare s2 "" in 
-                    match s1_empty, s2_empty with 
-                    | 0, _ -> None 
-                    | _, 0 -> None 
+                 (fun (s1, s2) ->
+                    let s1_empty = String.compare s1 "" in
+                    let s2_empty = String.compare s2 "" in
+                    match s1_empty, s2_empty with
+                    | 0, _ -> None
+                    | _, 0 -> None
                     | _ -> Some (s1, s2))
                  "construct case (c)" "case_input" 99);
             ]
@@ -385,7 +384,7 @@ module View = struct
 end
 
 
-let doc = Dom_html.document  
+let doc = Dom_html.document
 
 let main _ =
   let parent =
@@ -399,7 +398,6 @@ let main _ =
 
 let _ = Dom_html.addEventListener
     doc
-    Dom_html.Event.domContentLoaded 
+    Dom_html.Event.domContentLoaded
     (Dom_html.handler main)
     Js._true
-
