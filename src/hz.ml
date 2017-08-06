cc81b228641885dd135d5993445916a38104774a
Joel Burget <joelburget@gmail.com>
2017-08-06 16:19:09 -0400

    First implementation of a vim mode.

--- src/hz.ml
+++ src/hz.ml
@@ -13,8 +13,8 @@ module Util = struct
 end
 
 module JSUtil = struct
-  let forceGetElementById id = 
-    let doc = Dom_html.document in  
+  let forceGetElementById id =
+    let doc = Dom_html.document in
     Js.Opt.get (doc##getElementById (Js.string id))
       (fun () -> assert false)
 
@@ -34,15 +34,15 @@ module JSUtil = struct
   let r_input id placeholder_str =
     let rs, rf = S.create "" in
     let i_elt = Html5.(input ~a:[
-        a_id id; 
-        a_class ["form-control"]; 
+        a_id id;
+        a_class ["form-control"];
         a_placeholder placeholder_str] ()) in
     let i_dom = To_dom.of_input i_elt in
     let _ = listen_to_t Ev.input i_dom (fun _ ->
         rf (Js.to_string (i_dom##.value))) in
     ((rs, rf), i_elt, i_dom)
 
-  module KeyCombo : 
+  module KeyCombo :
   sig
     type t
     val make : string -> int -> t
@@ -61,28 +61,29 @@ module JSUtil = struct
 
     let enter = _kc "Enter" 13
     let esc = _kc "Esc" 27
-    let number_1 = _kc "1" 49
-    let number_2 = _kc "2" 50
-    let number_3 = _kc "3" 51
-    let p = _kc "p" 112
+    let h = _kc "h" 104
+    let j = _kc "j" 106
+    let k = _kc "k" 107
+    let l = _kc "l" 108
+    let capL = _kc "L" 76
+    let capR = _kc "R" 82
     let x = _kc "x" 120
-    let greaterThan = _kc ">" 62 
-    let n = _kc "n" 110 
-    let s = _kc "s" 115 
-    let dot = _kc "." 46 
-    let colon = _kc ":" 58 
-    let v = _kc "v" 118 
-    let backslash = _kc "\\" 92 
-    let openParens = _kc "(" 40 
-    let pound = _kc "#" 35 
-    let plus = _kc "+" 43 
-    let l = _kc "l" 108 
-    let r = _kc "r" 114 
-    let c = _kc "c" 99 
+    let greaterThan = _kc ">" 62
+    let n = _kc "n" 110
+    let s = _kc "s" 115
+    let dot = _kc "." 46
+    let colon = _kc ":" 58
+    let v = _kc "v" 118
+    let backslash = _kc "\\" 92
+    let openParens = _kc "(" 40
+    let pound = _kc "#" 35
+    let plus = _kc "+" 43
+    let r = _kc "r" 114
+    let c = _kc "c" 99
     let qmark = _kc "?" 63
   end
 
-  let get_keyCode (evt : Dom_html.keyboardEvent Js.t) = 
+  let get_keyCode (evt : Dom_html.keyboardEvent Js.t) =
     Js.Optdef.get (evt##.which) (fun () -> assert false)
 end
 
@@ -95,8 +96,8 @@ module ActionPalette = struct
     let doAction action =
       rf (Action.performSyn Ctx.empty action (React.S.value rs)) in
 
-    let module KC = JSUtil.KeyCombo in 
-    let module KCs = JSUtil.KeyCombos in 
+    let module KC = JSUtil.KeyCombo in
+    let module KCs = JSUtil.KeyCombos in
 
     (* helper function for constructing action buttons with no textbox *)
     let action_button action btn_label key_combo =
@@ -121,9 +122,9 @@ module ActionPalette = struct
                pcdata (btn_label ^ " [" ^ (KC.to_string key_combo) ^ "]")]) in
 
     (* actions that take an input. the conversion function
-     * goes from a string (the input value) to an arg option 
+     * goes from a string (the input value) to an arg option
      * where arg is the action argument. *)
-    let action_input_button action conv btn_label input_id key_combo placeholder_str = 
+    let action_input_button action conv btn_label input_id key_combo placeholder_str =
       (* create reactive input box *)
       let (i_rs, i_rf), i_elt, i_dom = JSUtil.r_input input_id placeholder_str in
       let clear_input () =
@@ -167,18 +168,18 @@ module ActionPalette = struct
       (* respond to enter and esc inside the input box *)
       let _ = JSUtil.listen_to Ev.keyup i_dom (fun evt ->
           let evt_key = JSUtil.get_keyCode evt in
-          if evt_key = (KC.keyCode KCs.enter) then 
+          if evt_key = (KC.keyCode KCs.enter) then
             begin
               button_dom##click;
               i_dom##blur;
               Js._false
             end
-          else if evt_key = (KC.keyCode KCs.esc) then 
+          else if evt_key = (KC.keyCode KCs.esc) then
             begin
               i_dom##blur;
               Js._false
             end
-          else Js._true) in 
+          else Js._true) in
       (* stop propagation of keys when focus is in input box *)
       let _ = JSUtil.listen_to Ev.keypress i_dom (fun evt ->
           Dom_html.stopPropagation evt; Js._true) in
@@ -191,7 +192,7 @@ module ActionPalette = struct
      * goes from a pair of strings to an arg option where arg is
      * the action argument. *)
     let action_input_input_button action conv btn_label input_id key_combo placeholder_str_1 placeholder_str_2 =
-      (* analagous to action_input_button, but with two input boxes. 
+      (* analagous to action_input_button, but with two input boxes.
        * could define an n-ary version of this, but this is probably more clear for now *)
       let input_id_1 = (input_id ^ "_1") in
       let input_id_2 = (input_id ^ "_2") in
@@ -228,7 +229,7 @@ module ActionPalette = struct
           let evt_key = JSUtil.get_keyCode evt in
           if evt_key = (KC.keyCode key_combo) then
             begin
-              Firebug.console##log "in c";  
+              Firebug.console##log "in c";
               i_dom_1##focus;
               Dom_html.stopPropagation evt;
               Js._false
@@ -238,29 +239,29 @@ module ActionPalette = struct
         ) in
       let i_keyup_listener i_dom = JSUtil.listen_to Ev.keyup i_dom (fun evt ->
           let evt_key = JSUtil.get_keyCode evt in
-          if evt_key = (KC.keyCode KCs.enter) then 
+          if evt_key = (KC.keyCode KCs.enter) then
             begin
               button_dom##click;
               i_dom##blur;
               Js._false
             end
-          else if evt_key = (KC.keyCode KCs.esc) then 
+          else if evt_key = (KC.keyCode KCs.esc) then
             begin
               i_dom##blur;
               Js._false
             end
-          else 
+          else
             begin
               Dom_html.stopPropagation evt; Js._true
             end
         ) in
       let _ = i_keyup_listener i_dom_1 in
       let _ = i_keyup_listener i_dom_2 in
-      let i_keypress_listener i_dom = 
+      let i_keypress_listener i_dom =
         JSUtil.listen_to Ev.keypress i_dom (fun evt ->
             Dom_html.stopPropagation evt; Js._true) in
-      let _ = i_keypress_listener i_dom_1 in 
-      let _ = i_keypress_listener i_dom_2 in 
+      let _ = i_keypress_listener i_dom_1 in
+      let _ = i_keypress_listener i_dom_2 in
       Html5.(div ~a:[a_class ["input-group"]] [
           span ~a:[a_class ["input-group-btn"]] [
             button_elt];
@@ -270,33 +271,33 @@ module ActionPalette = struct
 
     (* now construct the action palette entries*)
     (* movement *)
-    let moveChild1 = action_button (Action.Move (Action.Child 1)) "move child 1" KCs.number_1 in 
-    let moveChild2 = action_button (Action.Move (Action.Child 2)) "move child 2" KCs.number_2 in 
-    let moveChild3 = action_button (Action.Move (Action.Child 3)) "move child 3" KCs.number_3 in 
-    let moveParent = action_button (Action.Move (Action.Parent)) "move parent" KCs.p in 
+    let moveDown = action_button (Action.Move Action.Down) "move down" KCs.j in
+    let moveLeft = action_button (Action.Move Action.Left) "move left" KCs.h in
+    let moveRight = action_button (Action.Move Action.Right) "move right" KCs.l in
+    let moveParent = action_button (Action.Move (Action.Parent)) "move parent" KCs.k in
 
     (* deletion *)
-    let delete = action_button (Action.Del) "del" KCs.x in 
+    let delete = action_button (Action.Del) "del" KCs.x in
 
     (* type construction *)
-    let constructArrow = action_button (Action.Construct Action.SArrow) "construct arrow" KCs.greaterThan in 
-    let constructNum = action_button (Action.Construct Action.SNum) "construct num" KCs.n in 
-    let constructSum = action_button (Action.Construct Action.SSum) "construct sum" KCs.s in 
+    let constructArrow = action_button (Action.Construct Action.SArrow) "construct arrow" KCs.greaterThan in
+    let constructNum = action_button (Action.Construct Action.SNum) "construct num" KCs.n in
+    let constructSum = action_button (Action.Construct Action.SSum) "construct sum" KCs.s in
 
     (* finishing *)
-    let finish = action_button (Action.Finish) "finish" KCs.dot in 
+    let finish = action_button (Action.Finish) "finish" KCs.dot in
 
     (* expression construction *)
-    let constructAsc = action_button (Action.Construct Action.SAsc) "construct asc" KCs.colon in 
+    let constructAsc = action_button (Action.Construct Action.SAsc) "construct asc" KCs.colon in
     let constructVar = action_input_button
         (fun v -> Action.Construct (Action.SVar v))
         (fun s -> match String.compare s "" with 0 -> None | _ -> Some s)
-        "construct var" "var_input" KCs.v "Enter var + press Enter" in 
+        "construct var" "var_input" KCs.v "Enter var + press Enter" in
     let constructLam = action_input_button
         (fun v -> Action.Construct (Action.SLam v))
         (fun s -> match String.compare s "" with 0 -> None | _ -> Some s)
-        "construct lam" "lam_input" KCs.backslash "Enter var + press Enter" in 
-    let constructAp = action_button (Action.Construct Action.SAp) "construct ap" KCs.openParens in 
+        "construct lam" "lam_input" KCs.backslash "Enter var + press Enter" in
+    let constructAp = action_button (Action.Construct Action.SAp) "construct ap" KCs.openParens in
     let constructLit = action_input_button
         (fun n -> Action.Construct (Action.SLit n))
         (fun s ->
@@ -304,10 +305,10 @@ module ActionPalette = struct
              let i = int_of_string s in
              if i < 0 then None else Some i
            with Failure _ -> None)
-        "construct lit" "lit_input" KCs.pound "Enter num + press Enter" in 
-    let constructPlus = action_button (Action.Construct Action.SPlus) "construct plus" KCs.plus in 
-    let constructInjL = action_button (Action.Construct (Action.SInj HExp.L)) "construct inj L" KCs.l in 
-    let constructInjR = action_button (Action.Construct (Action.SInj HExp.R)) "construct inj R" KCs.r in 
+        "construct lit" "lit_input" KCs.pound "Enter num + press Enter" in
+    let constructPlus = action_button (Action.Construct Action.SPlus) "construct plus" KCs.plus in
+    let constructInjL = action_button (Action.Construct (Action.SInj HExp.L)) "construct inj L" KCs.capL in
+    let constructInjR = action_button (Action.Construct (Action.SInj HExp.R)) "construct inj R" KCs.capR in
     let constructCase = action_input_input_button
         (fun (v1,v2) -> Action.Construct (Action.SCase (v1,v2)))
         (fun (s1, s2) ->
@@ -317,10 +318,10 @@ module ActionPalette = struct
            | 0, _ -> None
            | _, 0 -> None
            | _ -> Some (s1, s2))
-        "construct case" "case_input" KCs.c 
+        "construct case" "case_input" KCs.c
         "Enter var + press Tab"
-        "Enter var + press Enter" in 
-    let constructNEHole = action_button (Action.Construct Action.SNEHole) "construct neHole" KCs.qmark in 
+        "Enter var + press Enter" in
+    let constructNEHole = action_button (Action.Construct Action.SNEHole) "construct neHole" KCs.qmark in
 
     (* finally, put it all together into the action palette *)
     Html5.(div ~a:[a_class ["row";"marketing"]] [
@@ -328,11 +329,11 @@ module ActionPalette = struct
           div ~a:[a_class ["panel";"panel-default"]] [
             div ~a:[a_class ["panel-title"]] [pcdata "Movement"];
             div ~a:[a_class ["panel-body"]] [
-              (moveChild1);
+              (moveDown);
               br ();
-              (moveChild2);
+              (moveLeft);
               br ();
-              (moveChild3);
+              (moveRight);
               br ();
               (moveParent);
             ]
@@ -397,9 +398,9 @@ module AppView = struct
     let zexp_view = (R.Html5.div (ReactiveData.RList.from_signal zexp_rs)) in
 
     (* htype view *)
-    let htype_rs = React.S.map (fun (_, htype) -> 
-        [HTMLView.of_htype htype]) rs in 
-    let htype_view = (R.Html5.div (ReactiveData.RList.from_signal htype_rs)) in 
+    let htype_rs = React.S.map (fun (_, htype) ->
+        [HTMLView.of_htype htype]) rs in
+    let htype_view = (R.Html5.div (ReactiveData.RList.from_signal htype_rs)) in
 
     Tyxml_js.To_dom.of_div Html5.(
         div [
@@ -409,9 +410,9 @@ module AppView = struct
               div ~a:[a_class ["logoDiv"]] [
                 img ~a:[a_id "logo"] ~alt:("Logo") ~src:(Xml.uri_of_string ("imgs/hazel-logo.png")) ()
               ]
-            ]; 
+            ];
             div ~a:[a_class ["subtext"]] [
-              pcdata "(a reference implementation of "; 
+              pcdata "(a reference implementation of ";
               a ~a:[a_href "https://arxiv.org/abs/1607.04180"] [pcdata "Hazelnut"]; pcdata ")"];
             div ~a:[a_class ["ModelExp"]] [zexp_view]; br ();
             div ~a:[a_class ["subtext"; "ModelType"]] [
@@ -420,12 +421,12 @@ module AppView = struct
           ActionPalette.make_palette (rs, rf);
           div ~a:[a_class ["container"]; a_id "footerContainer"] [
             p [
-              pcdata "Source (OCaml): "; 
+              pcdata "Source (OCaml): ";
               a ~a:[a_href "https://github.com/hazelgrove/HZ"] [
                 pcdata "https://github.com/hazelgrove/HZ"
               ]];
             p [
-              pcdata "A project of "; 
+              pcdata "A project of ";
               a ~a:[a_href "http://hazelgrove.org"] [
                 pcdata "Hazel Grove"
               ];
@@ -435,11 +436,11 @@ module AppView = struct
 end
 
 (* execution starts here *)
-let _ = JSUtil.listen_to_t 
+let _ = JSUtil.listen_to_t
     Dom_html.Event.domContentLoaded
     Dom_html.document
-    (fun _ -> 
+    (fun _ ->
        let rs, rf = React.S.create (Model.empty) in
-       let parent = JSUtil.forceGetElementById "container" in 
+       let parent = JSUtil.forceGetElementById "container" in
        Dom.appendChild parent (AppView.view (rs, rf)))
 
